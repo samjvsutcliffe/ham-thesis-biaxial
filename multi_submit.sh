69b15b9c5cf39de750cd41e296f9e3db8348feb9
Sam <sam.sutcliffe@durham.ac.uk>
2026-04-03 17:07:57 +0100

    Added biaxial test

--- multi_submit.sh
+++ multi_submit.sh
@@ -4,20 +4,20 @@
 export REFINE=1
 read -p "Do you want to clear previous data? (y/n)" yn
 case $yn in
-    [yY] ) echo "Removing data";rm data/*; break;;
-    [nN] ) break;;
+    [yY] ) echo "Removing data";rm -r data/*; break;;
+    qnN] ) break;;
 esac
 set -e
 module load aocc/5.0.0
 module load aocl/5.0.0
 sbcl --dynamic-space-size 16000 --load "build.lisp" --quit
 
-for h in 50 100 150
-do
-    for f in 0.4 0.3 0.2 0.1 0
-    do
-        export HEIGHT=$h
-        export FLOATATION=$f
-        sbatch batch_template_worker.sh
-    done
-done
+export REFINE=1
+export ANGLE=15
+sbatch batch_bi.sh
+export ANGLE=30
+sbatch batch_bi.sh
+export ANGLE=45
+sbatch batch_bi.sh
+export ANGLE=60
+sbatch batch_bi.sh
